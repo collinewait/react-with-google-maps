5d20f42027f89a5c295bd81d696af31b37cd0c81
collinewait <collinewait17@gmail.com>
2019-09-19 15:31:38 +0300

    display skateboard parks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker
+} from "react-google-maps";
+import * as parksData from "./data/skateboard-parks.json";
 
 function Map() {
   return (
     <GoogleMap
       defaultZoom={10}
       defaultCenter={{ lat: 45.420422, lng: -75.692429 }}
-    />
+    >
+      {parksData.features.map(park => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={{
+            lat: park.geometry.coordinates[1],
+            lng: park.geometry.coordinates[0]
+          }}
+        />
+      ))}
+    </GoogleMap>
   );
 }
 
